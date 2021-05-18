6c29e3854c00069296db2ce947278a2b9d41ee61
ThihatrangNguyen1995 <nguy0823@algonquinlive.com>
2021-05-18 14:40:08 -0400

    add scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
+import NarBar from "./components/NarBar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-        <div className="sideBar"></div>
-        <div className="main-content"></div>
+        <div className="sideBar">
+            <NarBar/>
+        </div>
+        <div className="main-content">
+            <div className="content">
+                <HomePage/>
+            </div>
+
+        </div>
     </div>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function HomePage() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am
+                    <span> Carol Nguyen.</span>
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia
+                    quibusdam. Blanditiis doloremque nemo ex facilis neque.
+                    A sint ipsam earum nemo omnis et.
+                </p>
+            </header>
         </div>
     )
 }
