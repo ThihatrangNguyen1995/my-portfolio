ce7c44ddfb1b891217d78fc4ec400eb2e1e1e95b
ThihatrangNguyen1995 <nguy0823@algonquinlive.com>
2021-05-19 12:38:17 -0400

    completed Home Page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,9 @@
 import React from "react";
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {Link} from "react-router-dom";
 
 function HomePage() {
     return (
@@ -8,12 +13,23 @@ function HomePage() {
                     Hi, I am
                     <span> Carol Nguyen.</span>
                 </h1>
-                <p>
+                <p className="h-sub-text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Cupiditate facere, numquam vitae placeat consequatur corrupti officia
                     quibusdam. Blanditiis doloremque nemo ex facilis neque.
                     A sint ipsam earum nemo omnis et.
                 </p>
+                <div className="icons">
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon fb"/>
+                    </Link >
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon ytb"/>
+                    </Link>
+                </div>
             </header>
         </div>
     )
