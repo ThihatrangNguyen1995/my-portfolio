bdd7fb550b7845c336a70c178b9af27901f4c2d2
ThihatrangNguyen1995 <nguy0823@algonquinlive.com>
2021-05-21 13:57:16 -0400

    completed Blogs Page

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import allBlogs from "../components/allBlogs";
+import Tittle from "../components/Tittle";
 
 function BlogsPage() {
     return (
-        <div className="BlogsPage">
-            <h1>Blogs Page</h1>
+
+        <div >
+            <div className="blog-title" >
+                <Tittle title={'Blogs'} span={'Blogs'}/>
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog) => {
+                        return <div className="blog" key={blog.id}>
+                            <div className="blog-content">
+                                <img src={blog.image} alt=""/>
+                                <a href={blog.link} className="blog-link">
+                                    {blog.title}
+                                </a>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+
         </div>
     )
 }
