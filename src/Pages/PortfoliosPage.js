86a2f57b8ccb38da3f10a9d2ca765316a519a825
ThihatrangNguyen1995 <nguy0823@algonquinlive.com>
2021-05-25 15:06:56 -0400

    completed PortfoliosPage

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Tittle from "../components/Tittle";
+import Categories from "../components/Categories";
+import MenuItems from "../components/MenuItems";
+import portfolios from "../components/allportfolios";
+
+const allCategories = ['All', ...new Set(portfolios.map(item => item.category))];
+//['All',...portfolios.map(item => item.category)]
+
 
 function PortfoliosPage() {
+    const [categories, setCategories] = useState(allCategories);
+    const [menuItems, setMenuItems] = useState(portfolios);
+
+    const filter = (category) => {
+        if(category === 'All'){
+            setMenuItems(portfolios)
+            return;
+        }
+        const filteredData = portfolios.filter((item)=>{
+            return item.category === category
+            })
+        setMenuItems(filteredData);
+    }
     return (
         <div className="PortfoliosPage">
-            <h1>Portfolios Page</h1>
+            <div className='title'>
+                <Tittle title={'Portfolios'} span={'Portfolios'}/>
+            </div>
+            <div className="portfolios-data">
+                <Categories filter = {filter} categories ={categories}/>
+                <MenuItems menuItem ={menuItems}/>
+            </div>
+
         </div>
     )
 }
